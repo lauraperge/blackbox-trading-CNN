cbd61ad057ea91517147faa51bbf80bde5173e2c
Unknown <laura.perge@gmail.com>
2019-03-04 17:33:10 +0100

    added report skeleton, notes

---
+++ utils/labels/trading_strategies.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+
+## Momentum-based
+## If it went wrong it goes wrong, if it went well it goes well
+
+## Moving average-based (MACD oscillator)
+## If short moving window avg > long moving window average: Buy
+## Not perfect, super easy, used by many
+
+
